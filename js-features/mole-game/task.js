289d2e34b51deba2d84232df7448fc2652a71378
Sofia Peturkina <peturkina@bk.ru>
2022-01-29 14:57:31 +0300

    changes made mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,18 +1,27 @@
 const deadCounter = document.getElementById("dead");
 const lostCounter = document.getElementById("lost");
-const hole = document.getElementById(`hole${i}`);
 
 for (let i = 1; i < 10; i++) {
-    hole.onckick = function() {
-        if (hole.className.includes("hole hole_has-mole")) {
-            deadCounter.textContent++;
-        } if (+deadCounter.textContent === 10) {
-            alert ("Поздравляем! Вы выйграли!");
+    const mole = document.getElementById(`hole${i}`);
+    const result = function(text) {
+        alert(text);
+        deadCounter.textContent = "0";
+        lostCounter.textContent = "0";
+    }
+    mole.onclick = function() {
+        if (mole.className === "hole hole_has-mole") {
+            deadCounter.textContent ++;
         } else {
-            lostCounter.textContent++;
-            if (+lostCounter.textContent === 5) {
-                alert("Вы проиграли :(")
-            } 
+            lostCounter.textContent ++;
+        }
+        if (deadCounter.textContent === '10') {
+            result("Вы выиграли!");	
+        } else if (lostCounter.textContent === '5') {
+            result("Вы проигали:с");
         }
     }
 }
+
+
+
+
